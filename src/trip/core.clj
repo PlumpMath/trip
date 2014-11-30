72a7db9069fe5558b7be186714645e0da8e71e7d
Johann Bestowrous <johann.bestowrous@gmail.com>
2014-11-30 10:54:27 -0800

    use defonce for better interop with lighttable

--- src/trip/core.clj
+++ src/trip/core.clj
@@ -8,40 +8,34 @@
 (def revise reset!)
 (def life put!)
 (def is (chan))
-(def quality (atom [255 255 255])) ;; defaults to white
+(defonce quality (atom [255 255 255])) ;; defaults to white
 (def qualia-source "https://docs.google.com/spreadsheets/d/1ShOWkEyJSkeFswHBq0kE9yh8fdxVYRXw2xPyOyfIirk/export?format=csv")
 
-
-(go-loop [_ (<! (async/timeout 0))]
-  (let [qualias (-> qualia-source slurp csv/read-csv)]
-    (defmacro reality [qualia]
+(defmacro reality [qualia]
       `(case ~qualia
          ~@(mapcat (fn [[name & colors]]
                      [name (mapv #(Integer/parseInt %) colors)])
-                   qualias))))
-  (recur (<! (async/timeout 300))))
+                   (-> qualia-source slurp csv/read-csv))))
 
 (go-loop [qualia (<! is)]
-  (let [test (reality qualia)]
-    (println test)
-      (->> (reality qualia)
-           (revise quality)))
+  (->> (reality qualia)
+       (revise quality))
   (recur (<! is)))
 
 (defn setup []
   (q/smooth))
 
-
 (defn draw []
   (apply q/background @quality))
 
+;; (life is "Turkey )
 
-
-(q/sketch
+(defonce
+  (q/sketch
      :renderer :opengl
      :setup setup
      :features [:present]
      :draw draw
-     :size :fullscreen)
+     :size :fullscreen))
 
 
